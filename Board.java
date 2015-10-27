f29befb7d72b492db55c6f37ca051ca2b7390c66
Jordi GUiu <guiu-3@hotmail.com>
2015-10-27 13:26:19 +0100

    Update Board.java

--- Board.java
+++ Board.java
@@ -6,7 +6,7 @@ public class Board{
     Arraylist<Arraylist<Cell>> board;
     
     public Board(int size) { // Fer la creadora
-        
+        ArrayList<ArrayList<Cell>> Taulell = new ArrayList<ArrayList<Cell>>();
     }
 
     public int getSize() {
