ee4eed981086b0eda7b96a005a054a4135d42bd1
Jordi GUiu <guiu-3@hotmail.com>
2015-10-27 13:21:07 +0100

    Versió 0

---
+++ Board.java
@@ -0,0 +1,27 @@
+package domini.Basic; //Falten fer els packages
+
+
+public class Board{
+    int size;
+    Arraylist<Arraylist<Cell>> board;
+    
+    public Board(int size) { // Fer la creadora
+        
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getCell(int column, int row) { // Com utilitzar column i row aquí que són de Cell?
+        return ;
+    }
+    
+    public void setCell(int column, int row) { // Com utilitzar column i row aquí que són de Cell?
+        this. = ;
+    }
+}
