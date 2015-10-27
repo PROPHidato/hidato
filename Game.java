27451be442901cb42d8cb88cd193b4e0d1410be4
alexct33 <alex.correa.t33@gmail.com>
2015-10-27 13:26:19 +0100

    Actualitzat amb totes les funcions de la classe

--- Game.java
+++ Game.java
@@ -2,9 +2,31 @@ package domini.Basic;
 
 public class Game {
   
-  public Game {
-    
+  int gameId;
+  int difficult;    // 1 = fàcil, 2 = mitjà, 3 = difícil
   
+  
+  public Game(int gameId, int difficult) {
+    
+    this.gameId = gameId;
+    this.difficult = difficult;
+
+  }
+  
+  public int getGameId() {
+    return gameId;
+  }
+  
+  public void setGameId(int gameId) {
+    this.gameId = gameId
+  }
+  
+  public int getDifficult() {
+    return difficult;
+  }
+  
+  public void setDifficult(int difficult) {
+    this.difficult = difficult;
   }
 
 
