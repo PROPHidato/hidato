7aa574ad5fc846a4956e33c8890f348d395b88c5
Jordi Guiu <guiu-3@hotmail.com>
2015-10-29 17:42:00 +0100

    Update Board.java

--- Board.java
+++ Board.java
@@ -4,7 +4,7 @@ package domini.Basic; //Falten fer els packages
 public class Board{
     int size;
     Arraylist<Arraylist<Cell>> board;
-    int max_annotations = size; //màxim nombre de marques que poden tenir les cel·es del taulell
+    int max_annotations = 9; //màxim nombre de marques que poden tenir les cel·es del taulell 
     
     public Board(int size) { // Fer la creadora
        board = new ArrayList<ArrayList<Cell>>(size);
