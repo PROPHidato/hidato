21a0b9d7eeb1289bceea36e1f182ff1b8e348e5a
Jordi Guiu <guiu-3@hotmail.com>
2015-10-29 17:42:26 +0100

    wow

--- BoardHidato.java
+++ BoardHidato.java
@@ -1,6 +1,5 @@
 package domini.Basic; //Falten fer els packages
 
-public class Board{
+public class BoardHidato extends Board{
     int max_annotations = size; //màxim nombre de marques que poden tenir les cel·es del taulell
-  
 }
