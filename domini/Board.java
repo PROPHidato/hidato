df0c7fb2ec7c48a094d4592580907befaff9bcf8
Miquel <xny556m@gmail.com>
2015-10-31 16:42:48 +0100

    int prova per mirar si puc fer commits

--- domini/Board.java
+++ domini/Board.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 
 public class Board {
     int size;
+    int prova; //per veure si puc fer commits
     ArrayList<ArrayList<Cell>> board;
     int max_annotations = 9; //màxim nombre de marques que poden tenir les cel·es del taulell
 
