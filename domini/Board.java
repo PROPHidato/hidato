ea3649d33fdf5d6c178b63532f98aa9ba3c252c0
Jou94 <j_jounou94@hotmail.com>
2015-10-31 16:45:55 +0100

    prova

--- domini/Board.java
+++ domini/Board.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 
 public class Board {
     int size;
+    //int prova;
     ArrayList<ArrayList<Cell>> board;
     int max_annotations = 9; //màxim nombre de marques que poden tenir les cel·es del taulell
 
