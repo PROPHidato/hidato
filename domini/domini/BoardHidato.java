6978e5bb50e6c1ef52f50d5e54b49f35964f0334
Miquel <xny556m@gmail.com>
2015-11-14 21:53:27 +0100

    nar fent. tot colocar celes invalides i la primera crec q ja va, almenys amb el que he anat provant

--- domini/domini/BoardHidato.java
+++ domini/domini/BoardHidato.java
@@ -26,13 +26,10 @@ public class BoardHidato extends Board {
             board.add(new ArrayList<CellHidato>(size));
             for (int j = 0; j < board.size(); ++j) {
                 board.get(i).add(new CellHidato(i,j));
-               /* if (i == 0 || j == 0 || i == board.size()-1 || j == board.size()-1)
-                    board.get(i).get(j).setValue(-1);
-            */
             }
         }
         max_annotations = size*size; //Si taulell hi han 25 caselles podrà anar-hi del 1 al 25 en una casella.
-        //colocar_celesinvalides();
+        colocar_celesinvalides();
     }
 
     //public boolean isStart(int row, int column) {return (board.get(row).get(column).getStart());}
@@ -40,6 +37,12 @@ public class BoardHidato extends Board {
 
     public static int consult_max_annotations(){
         return max_annotations;
+    }
+
+    public static void generar_written(){//posem al taulell les celes que al ppi estaran escrites
+        int numcela,numvisibles;
+        numcela = numvisibles = 1; //comencem per la 1 i amb 1 cela visible
+
     }
 
     static boolean posarainvalida(int row,int column) {//si totes les celes veines o totes menys una son invalides, return true
@@ -109,7 +112,6 @@ public class BoardHidato extends Board {
         while (posades < maxinvalides) {
             int row = posi.nextInt(ncelesvalT);   //fila random entre totes les celes totals
             int column = posj.nextInt(ncelesvalT); //columan random entre totes les celes totals
-            //if (invalidesveines(Taulell,row,column)) {
             if (getValidaCell(row, column)) {   //la cela q hem triat aleatoriament, si no es invalida la posem
                 // la posem a invalida
                 System.out.println("fila " + row + " columna " + column);
@@ -117,11 +119,27 @@ public class BoardHidato extends Board {
                 ++posades;
             }
         }
+        boolean posatinici = false;
+        Random posiini = new Random();
+        Random posjini = new Random();
+        int rowini, columnini;
+        while(!posatinici) {
+            rowini = posi.nextInt(ncelesvalT);
+            columnini = posj.nextInt(ncelesvalT);
+            if (getValidaCell(rowini,columnini)){
+                board.get(rowini).get(columnini).setValue(1);
+                board.get(rowini).get(columnini).switchWritten();
+                setStart_i(rowini);
+                setStart_j(columnini);
+                posatinici = true;
+            }
+        }
         for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
                 if (!getValidaCell(i, j)) {//si no es invalida mirem les del seu voltant
                     //si totes o totes menys una son invalides, la canviem a invalida tambe
                     if (posarainvalida(i, j))
+                        System.out.println("posem a invalida la casella " + i + " " + j);
                         switchValidaCell(i, j);
                 }
             }
@@ -146,19 +164,19 @@ public class BoardHidato extends Board {
 
     public CellHidato getCell(int i, int j) {return board.get(i).get(j);}
 
-    public void setStart_i(int i) {this.start_i = i;}
+    public static void setStart_i(int i) {start_i = i;}
 
-    public int getStart_i() {return start_i;}
+    public static int getStart_i() {return start_i;}
 
-    public void setStart_j(int j) {this.start_j = j;}
+    public static void setStart_j(int j) {start_j = j;}
 
-    public int getStart_j() {return start_j;}
+    public static int getStart_j() {return start_j;}
 
-    public void setFinish_i(int i) {this.finish_i = i;}
+    public static void setFinish_i(int i) {finish_i = i;}
 
-    public int getFinish_i() {return finish_i;}
+    public static int getFinish_i() {return finish_i;}
 
-    public void setFinish_j(int j) {this.finish_j = j;}
+    public static void setFinish_j(int j) {finish_j = j;}
 
     public int getFinish_j() {return finish_j;}
 
